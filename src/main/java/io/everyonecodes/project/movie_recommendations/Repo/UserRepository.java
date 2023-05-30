864d65a782805d936389979efe2f4d81360af8b8
Keinam Shegow <shegow.keinam@gmail.com>
2023-05-30 13:53:45 +0200

    added username query to repository

--- src/main/java/io/everyonecodes/project/movie_recommendations/Repo/UserRepository.java
+++ src/main/java/io/everyonecodes/project/movie_recommendations/Repo/UserRepository.java
@@ -3,5 +3,8 @@ package io.everyonecodes.project.movie_recommendations.Repo;
 import io.everyonecodes.project.movie_recommendations.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
