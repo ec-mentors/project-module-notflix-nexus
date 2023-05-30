63a2e6ec08a6a7e74448de281db8ad91e5be5f20
benedikt <benedikt@domain.com>
2023-05-30 11:56:20 +0200

    module set up

---
+++ src/main/java/io/everyonecodes/project/movie_recommendations/MovieRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.project.movie_recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommendationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/everyonecodes/project/movie_recommendations/MovieRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.project.movie_recommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
