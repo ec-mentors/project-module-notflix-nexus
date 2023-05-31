3d8b6c223b6d4cd6ed4d3f2d6616324641f293f2
Thomas Kostic <kosticthomas18@gmail.com>
2023-05-31 13:22:17 +0200

    added test classes

---
+++ src/test/java/io/everyonecodes/project/movie_recommendations/logic/MovieServiceTest.java
@@ -0,0 +1,20 @@
+package io.everyonecodes.project.movie_recommendations.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+    @Test
+    void findAllMovies() {
+    }
+
+    @Test
+    void changeMovie() {
+    }
+
+    @Test
+    void addMovie() {
+    }
+}
