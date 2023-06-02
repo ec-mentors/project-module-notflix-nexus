b6406e261d22bb15856d62c49974a5b491fe4681
benedikt <benedikt@domain.com>
2023-06-02 10:55:14 +0200

    fix some tests From UserServiceTest

--- src/test/java/io/everyonecodes/project/movie_recommendations/logic/UserServiceTest.java
+++ src/test/java/io/everyonecodes/project/movie_recommendations/logic/UserServiceTest.java
@@ -1,5 +1,6 @@
 package io.everyonecodes.project.movie_recommendations.logic;
 
+import io.everyonecodes.project.movie_recommendations.configuration.DefaultUserRunner;
 import io.everyonecodes.project.movie_recommendations.persistance.domain.Movie;
 import io.everyonecodes.project.movie_recommendations.persistance.domain.UserEntity;
 import io.everyonecodes.project.movie_recommendations.persistance.domain.WatchList;
@@ -7,6 +8,7 @@ import io.everyonecodes.project.movie_recommendations.persistance.repository.Use
 import io.everyonecodes.project.movie_recommendations.persistance.repository.WatchListRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,14 +35,20 @@ class UserServiceTest {
     @MockBean
     PasswordEncoder encoder;
 
-    @MockBean
-    WatchListRepository watchListRepository;
+//    @MockBean
+//    WatchListRepository watchListRepository;
 
     @MockBean
-    MovieService movieService;
+    WatchListService watchListService;
+
+//    @MockBean
+//    MovieService movieService;
 
     @MockBean
     SecurityFilterChain securityFilterChain;
+
+    @MockBean
+    DefaultUserRunner defaultUserRunner;
 
     private final String username = "testuser";
     private final String password = "password";
@@ -59,23 +67,20 @@ class UserServiceTest {
     @Test
     public void testAddUserIfDoesNotExist() {
         UserEntity user = new UserEntity();
-        user.setUsername(username);
         user.setPassword(password);
 
         when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
         when(encoder.encode(user.getPassword())).thenReturn("encodedPassword");
-        when(watchListRepository.save(any(WatchList.class))).thenReturn(new WatchList());
-        when(userRepository.save(user)).thenReturn(user);
 
         UserEntity result = userService.addUser(user);
 
         assertEquals(user, result);
+        assertEquals("encodedPassword", user.getPassword());
         verify(userRepository).findByUsername(user.getUsername());
-        verify(encoder).encode(user.getPassword());
-        verify(watchListRepository).save(any(WatchList.class));
+        verify(encoder).encode(password);
+        verify(watchListService).createNewWatchList();
         verify(userRepository).save(user);
     }
-
 
     @Test
     public void testGetWatchListByUsernameIfExists() {
@@ -111,21 +116,14 @@ class UserServiceTest {
         UserEntity user = new UserEntity();
         user.setUsername(username);
         WatchList watchList = new WatchList();
+        watchList.setId(123L);
         user.setWatchList(watchList);
-        user.addMovieToWatchList(new Movie());
 
         when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
-        when(movieService.addMovie(movie)).thenReturn(movie);
-        when(watchListRepository.save(watchList)).thenReturn(watchList);
-        when(userRepository.save(user)).thenReturn(user);
 
-        Movie result = userService.addToWatchListByUsername(username, movie);
+        userService.addToWatchListByUsername(username, movie);
 
-        assertEquals(movie, result);
-        assertEquals(2, user.getWatchList().getMovies().size());
         verify(userRepository).findByUsername(username);
-        verify(movieService).addMovie(movie);
-        verify(watchListRepository).save(watchList);
-        verify(userRepository).save(user);
+        verify(watchListService).addMovieById(Mockito.anyLong(), Mockito.any(Movie.class));
     }
 }
