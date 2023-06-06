e965b9fe91b5e6fc72b89f455ca4235a591a9692
benedikt <benedikt@domain.com>
2023-06-06 13:50:13 +0200

    findMovieByImdbId searches MySQL-db first and adds unknown movies to it

--- src/main/java/io/everyonecodes/project/movie_recommendations/communication/endpoint/MovieEndpoint.java
+++ src/main/java/io/everyonecodes/project/movie_recommendations/communication/endpoint/MovieEndpoint.java
@@ -25,12 +25,6 @@ public class MovieEndpoint {
         return movieService.addMovie(movie);
     }
 
-//    @GetMapping("/{movieId}")
-//    @Secured("ROLE_ADMIN")
-//    Optional<Movie> getMovieById(@PathVariable Long movieId) {
-//        return movieService.findMovieById(movieId);
-//    }
-
     @GetMapping("/{imdbId}")
     Optional<Movie> getMovieByImdbId(@PathVariable String imdbId) {
         return movieService.findMovieByImdbId(imdbId);
