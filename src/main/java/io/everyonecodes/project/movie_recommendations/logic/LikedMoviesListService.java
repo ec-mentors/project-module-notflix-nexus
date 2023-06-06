8172f10792e84d58eeed87b3523adce71d612105
Thomas Kostic <kosticthomas18@gmail.com>
2023-06-06 14:15:56 +0200

    added missing classes

---
+++ src/main/java/io/everyonecodes/project/movie_recommendations/logic/LikedMoviesListService.java
@@ -0,0 +1,69 @@
+package io.everyonecodes.project.movie_recommendations.logic;
+
+import io.everyonecodes.project.movie_recommendations.persistance.domain.LikedMoviesList;
+import io.everyonecodes.project.movie_recommendations.persistance.domain.Movie;
+import io.everyonecodes.project.movie_recommendations.persistance.repository.LikedMoviesListRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+@Service
+public class LikedMoviesListService {
+    private final MovieService movieService;
+    private final LikedMoviesListRepository likedMoviesListRepository;
+    private final String failMessage;
+
+    public LikedMoviesListService(MovieService movieService, LikedMoviesListRepository likedMoviesListRepository, @Value("${notflix.fail.message") String failMessage) {
+        this.movieService = movieService;
+        this.likedMoviesListRepository = likedMoviesListRepository;
+        this.failMessage = failMessage;
+    }
+
+    public LikedMoviesList createNewLikedMoviesList() {
+        return likedMoviesListRepository.save(new LikedMoviesList());
+    }
+
+    public List<LikedMoviesList> findAllLikedMoviesLists() {
+        return likedMoviesListRepository.findAll();
+    }
+
+    public Optional<LikedMoviesList> findLikedMoviesListById(Long id) {
+        return likedMoviesListRepository.findById(id);
+    }
+
+    public Movie addMovieById(Long likedMoviesListId, Movie movie) {
+        Movie returnedMovie = movieService.addMovie(movie);
+        changeIfPresentById(likedMoviesListId, likedMoviesList -> {
+            if (!likedMoviesList.getMovies().contains(returnedMovie)) likedMoviesList.addMovie(movie);
+        });
+        return returnedMovie;
+    }
+
+    public void clearLikedMoviesListById(Long likedMoviesListId) {
+        changeIfPresentById(likedMoviesListId, LikedMoviesList::clear);
+    }
+
+    public void removeMovieByIds(Long likedMoviesListId, Long movieId) {
+        changeIfPresentById(likedMoviesListId, likedMoviesList -> likedMoviesList.removeMovieById(movieId));
+    }
+
+    private void changeIfPresentById(Long LikedMoviesListId, Consumer<LikedMoviesList> change) {
+        Optional<LikedMoviesList> optionalLikedMoviesList = likedMoviesListRepository.findById(LikedMoviesListId);
+        optionalLikedMoviesList.ifPresent(likedMoviesList -> {
+            change.accept(likedMoviesList);
+            likedMoviesListRepository.save(likedMoviesList);
+        });
+    }
+
+//    public Movie addMovieByImdbId(Long likedMoviesListId, String imdbId) {
+//        Movie returnedMovie = movieService.findMovieByImdbId();
+//        changeIfPresentById(likedMoviesListId, likedMoviesList -> {
+//            if (!likedMoviesList.getLikedMovies().contains(returnedMovie)) likedMoviesList.addMovie(returnedMovie);
+//        });
+//        return returnedMovie;
+//    }
+
+}
---
+++ src/main/java/io/everyonecodes/project/movie_recommendations/persistance/domain/LikedMoviesList.java
@@ -0,0 +1,45 @@
+package io.everyonecodes.project.movie_recommendations.persistance.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+public class LikedMoviesList {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Movie> movies = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+    public void removeMovieById(Long id) {
+        Optional<Movie> optionalMovie = movies.stream().filter(movie -> id.equals(movie.getId())).findFirst();
+        optionalMovie.ifPresent(movie -> movies.remove(movie));
+    }
+
+    public void clear() {
+        movies.clear();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+}
