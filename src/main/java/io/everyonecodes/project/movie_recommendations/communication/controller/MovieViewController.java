6d41aabc945380697d7ab6113fc48705463c35c8
Keinam Shegow <daijirobi@gmail.com>
2023-06-22 14:01:21 +0200

    Displaying recommendations in a html template works

---
+++ src/main/java/io/everyonecodes/project/movie_recommendations/communication/controller/MovieViewController.java
@@ -0,0 +1,30 @@
+package io.everyonecodes.project.movie_recommendations.communication.controller;
+
+import io.everyonecodes.project.movie_recommendations.logic.MovieService;
+import io.everyonecodes.project.movie_recommendations.persistance.domain.Movie;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/movies")
+public class MovieViewController {
+    private final MovieService movieService;
+
+    public MovieViewController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/{movieId}/recommendationsById")
+    public String getRecommendationsById(@PathVariable String movieId, Model model) {
+        var inputMovie = movieService.findMovieByTmdbId(movieId);
+        List<Movie> movies = movieService.findRecommendationsById(movieId);
+        model.addAttribute("movies", movies);
+        model.addAttribute("inputMovie", inputMovie.get().getTitle());
+        return "recommendations";
+    }
+}
